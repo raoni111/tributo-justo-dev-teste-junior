86ad1a9026312ce888dfe6f8d7f23a03962a00c8
Raoni Oliveira Quevedo <Raoni.itar@gmail.com>
2025-04-23 08:54:07 -0300

    implement-enterprise-service-into-routes

--- routes/enterprise_routes.py
+++ routes/enterprise_routes.py
@@ -1,9 +1,16 @@
+from model.enterprise import EnterpriseBaseModel
+from service.enterprise_service import EnterpriseService
+
 from fastapi import APIRouter
 
 enterpriseRouter = APIRouter()
 
+# Rota response: retornar todas as empresas do banco de dados
 @enterpriseRouter.get("/enterprise", tags=["enterprise"])
-def get_companies():
-    return {
-        "teste": "teste"
-    }
+async def get_companies():
+    companies: list[EnterpriseBaseModel] = []
+    enterprise_service = EnterpriseService()
+    
+    companies = enterprise_service.get_all_companies()
+    
+    return companies
